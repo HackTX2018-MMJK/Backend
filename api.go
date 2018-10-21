ba3e0f6941aefd4140ba297255ab869a8fe74ede
Mason Davis <mason.davis@utexas.edu>
2018-10-21 02:18:43 -0500

    backend done?

--- api.go
+++ api.go
@@ -30,6 +30,12 @@ func handle_requests(mappings map[string]string) {
 
 	/* Routes */
 	router.HandleFunc("/seats/{uuid}", SeatHandler).Methods("GET")
+	router.HandleFunc("/rr/enqueue", EnqueueHandler).Methods("POST").
+		Headers("Content-Type", "application/json")
+	router.HandleFunc("/rr/status/{uuid}", StatusHandler).Methods("GET")
+	router.HandleFunc("/rr/size", SizeHandler).Methods("GET")
+	router.HandleFunc("/rr/dequeue", DequeueHandler).Methods("POST").
+		Headers("Content-Type", "application/json")
 
 	/* Web Server */
 	timeout := time.Second * 15
@@ -69,15 +75,58 @@ func handle_requests(mappings map[string]string) {
 	os.Exit(0)
 }
 
-func SeatHandler(w http.ResponseWriter, r *http.Request) {
+func DequeueHandler(w http.ResponseWriter, r *http.Request) {
 	/* Extract values */
-	vars := mux.Vars(r)
-	token, exists := tokens[vars["uuid"]]
+	var body StringResponse
+
+	/* Parse POSTED data */
+	if r.Body == nil {
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity),
+			http.StatusUnprocessableEntity)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity),
+			http.StatusUnprocessableEntity)
+		return
+	}
+
+	token := body.Response
+	_, exists := tokens[token]
 
 	if exists {
-		respones := StringResponse{Response: token}
+		Remove_waiter(token)
+	} else {
+		http.NotFound(w, r)
+	}
+}
 
-		b, err := json.Marshal(respones)
+func SizeHandler(w http.ResponseWriter, r *http.Request) {
+	response := IntResponse{Response: Get_length()}
+
+	b, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type:", "application/json")
+	w.Write(b)
+}
+
+func StatusHandler(w http.ResponseWriter, r *http.Request) {
+	/* Extract values */
+	vars := mux.Vars(r)
+
+	token := vars["uuid"]
+	_, exists := tokens[vars["uuid"]]
+
+	if exists {
+		response := IntResponse{Response: Get_position(token)}
+
+		b, err := json.Marshal(response)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -88,6 +137,60 @@ func SeatHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.NotFound(w, r)
 	}
+
+}
+
+func EnqueueHandler(w http.ResponseWriter, r *http.Request) {
+	/* Extract values */
+	var body StringResponse
+
+	/* Parse POSTED data */
+	if r.Body == nil {
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity),
+			http.StatusUnprocessableEntity)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity),
+			http.StatusUnprocessableEntity)
+		return
+	}
+
+	token := body.Response
+	_, exists := tokens[token]
+
+	if exists {
+		Add_waiter(token)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+func SeatHandler(w http.ResponseWriter, r *http.Request) {
+	/* Extract values */
+	vars := mux.Vars(r)
+	token, exists := tokens[vars["uuid"]]
+
+	if exists {
+		response := StringResponse{Response: token}
+
+		b, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type:", "application/json")
+		w.Write(b)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+type IntResponse struct {
+	Response int `json "response"`
 }
 
 type StringResponse struct {
--- bathroom_util.go
+++ bathroom_util.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "strings"
+	"strings"
 )
 
 var waiters []string
@@ -9,32 +9,40 @@ var fields []string
 var input string
 
 // Add id to the end of waiters
-func Add_waiter (id string) { 
-    if Get_position(id) == -1 { waiters = append (waiters, id) }
+func Add_waiter(id string) {
+	if Get_position(id) == -1 {
+		waiters = append(waiters, id)
+	}
 }
 
-// Remove element from waiters with val = id if it exists. Return true if value 
+// Remove element from waiters with val = id if it exists. Return true if value
 // removed was the first element
-func Remove_waiter (id string) bool {
-    index := Get_position(id)
-    if index != -1 { waiters = append(waiters[:index], waiters[index+1:]...) }
-    return index == 0
+func Remove_waiter(id string) bool {
+	index := Get_position(id)
+	if index != -1 {
+		waiters = append(waiters[:index], waiters[index+1:]...)
+	}
+	return index == 0
 }
 
 // Return first element in waiters or "" if waiters is empty
-func Get_front () string {
-    if len(waiters) > 0 { return waiters[0] }
-    return ""
+func Get_front() string {
+	if len(waiters) > 0 {
+		return waiters[0]
+	}
+	return ""
 }
 
 // Return the position of the id in waiters, -1 otherwise
-func Get_position (id string) int {
-    var index int                                                               
-    for index = 0; index < len(waiters); index++ {                              
-        if strings.Compare(id, waiters[index]) == 0 { return index }
-    }
-    return -1
+func Get_position(id string) int {
+	var index int
+	for index = 0; index < len(waiters); index++ {
+		if strings.Compare(id, waiters[index]) == 0 {
+			return index
+		}
+	}
+	return -1
 }
 
 // Return the number of elements in waiters
-func Get_length () int { return len(waiters) }
+func Get_length() int { return len(waiters) }
