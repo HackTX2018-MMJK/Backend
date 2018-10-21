8557b557010e6db16c750f088e21cb2ec994f723
phleisch <kaifleischmans@gmail.com>
2018-10-21 01:41:34 -0500

    Added get length and get position

--- bathroom_util.go
+++ bathroom_util.go
@@ -14,19 +14,25 @@ func Add_waiter (id string) { waiters = append (waiters, id) }
 // Remove element from waiters with val = id if it exists. Return true if value 
 // removed was the first element
 func Remove_waiter (id string) bool {
-    var index int
-    for index = 0; index < len(waiters); index++ {
-        if strings.Compare(id, waiters[index]) == 0 {
-            waiters = append(waiters[:index], waiters[index+1:]...)
-            break
-        }
-    }
-
+    index := Get_position(id)
+    if index != -1 { waiters = append(waiters[:index], waiters[index+1:]...) }
     return index == 0
 }
 
 // Return first element in waiters or "" if waiters is empty
-func Get_Front () string {
+func Get_front () string {
     if len(waiters) > 0 { return waiters[0] }
     return ""
 }
+
+// Return the position of the id in waiters, -1 otherwise
+func Get_position (id string) int {
+    var index int                                                               
+    for index = 0; index < len(waiters); index++ {                              
+        if strings.Compare(id, waiters[index]) == 0 { return index }
+    }
+    return -1
+}
+
+// Return the number of elements in waiters
+func Get_length () int { return len(waiters) }
